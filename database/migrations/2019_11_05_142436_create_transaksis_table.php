73dcc7d3f920fffa507b4db1f11810d619c6ea9f
hasyrawi@gmail.com <hasyrawi@gmail.com>
2019-11-05 22:42:28 +0800

    add migratioln

---
+++ database/migrations/2019_11_05_142436_create_transaksis_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('rent_id');
+            $table->unsignedBigInteger('car_id');
+            $table->unsignedBigInteger('pelanggan_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksis');
+    }
+}
---
+++ database/migrations/2019_11_05_142614_create_pelanggans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePelanggansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pelanggans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name_depan'); 
+            $table->string('nama_belakang');
+            $table->string('alamat'); 
+            $table->string('no_hp');
+            $table->string('foto');
+            $table->string('email');
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pelanggans');
+    }
+}
