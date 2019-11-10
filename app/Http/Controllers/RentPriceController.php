df40e2e054a1eaa43c190b9f4d72afcd735ea981
hasyrawi@gmail.com <hasyrawi@gmail.com>
2019-11-11 02:16:48 +0800

    Some Update

---
+++ app/Http/Controllers/RentPriceController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RentPriceController extends Controller
+{
+    /**
+     * @param id
+     */
+
+     public function updatePrices($id) { 
+
+     }
+     /**
+      * @param id
+      */
+     public function deletePrices($id){ 
+
+     }
+}
--- app/Http/Controllers/Transaksi.php
+++ app/Http/Controllers/Transaksi.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Transaksi as Trans;
 class Transaksi extends Controller
 {
-    //
+    
+    /**
+     * @param id
+     * @request dari Facades Request 
+     * @return [json]
+     */
+    public function addTrans(){ 
+        $trans = new Trans() ; 
+    }
 }
